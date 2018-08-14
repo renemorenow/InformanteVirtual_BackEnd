06b75d257f2d82c776217b07a600b4fd8de21f7c
Mauro Vizcaino Rodriguez <lex1802@gmail.com>
2018-08-14 07:33:41 -0500

    08142018 consumo del servicio del home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { DescriptionPage } from '../description/description';
+import { PlistarcasosProvider } from '../../providers/plistarcasos/plistarcasos';
+import { Subscriber } from '../../../node_modules/rxjs/Subscriber';
 
 @Component({
   selector: 'page-home',
@@ -8,11 +10,21 @@ import { DescriptionPage } from '../description/description';
 })
 
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+casos
+  constructor(public navCtrl: NavController, public provedor:PlistarcasosProvider) {
 
   }
   abrirDescription() {
     this.navCtrl.push(DescriptionPage);
-   }
+  }
+
+  ionViewDidLoad(){
+    this.provedor.obtenerdatos( )
+    .subscribe(
+      (data)=>{this.casos=data;},
+      (error)=>{console.log(error);}
+    )
+    
+  }
+
 }
---
+++ src/providers/plistarcasos/plistarcasos.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+/*
+  Generated class for the PlistarcasosProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class PlistarcasosProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello PlistarcasosProvider Provider');
+  }
+
+    obtenerdatos(){
+      return this.http.get('https://apiadenunciarrnmc.policia.gov.co/wsInformante/api/Investigations');
+    }
+
+  }
